afa8a40bd25c75caa9b98be73000c489c64e0563
brianborchert <brian@capitano.dk>
2023-11-13 08:43:56 +0100

    Almost everything except for the smileys themselves done

---
+++ Smileys/Controllers/SmileyExplanationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Smileys.Controllers
+{
+    public class SmileyExplanationController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Smileys/Models/Company.cs
+++ Smileys/Models/Company.cs
@@ -5,12 +5,26 @@ namespace Smileys.Models
     public class Company
     {
         public int Id { get; set; }
+
+        [Display(Name = "Navn")]
         public string Name { get; set; }
+
+        [Display(Name = "CVR-nr.")]
         public int CVR { get; set; }
+
+        [Display(Name = "Vejnavn")]
         public string Street { get; set; }
+
+        [Display(Name = "Husnr.")]
         public string HouseNumber { get; set; }
+
+        [Display(Name = "Postnr.")]
         public int ZipCode { get; set; }
+
+        [Display(Name = "By")]
         public string City { get; set; }
+
+        [Display(Name = "Smiley'er")]
         public string Smileys { get; set; }
     }
 }
